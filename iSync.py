b29e62814f40fa88f34cdd6a078a9d53f1374b78
lastre <orionbl@hotmail.es>
2016-05-29 17:05:33 +0200

    [iSync] refactor roles

--- iSync.py
+++ iSync.py
@@ -8,146 +8,232 @@ __license__= ""
 __version__= "0.0.1"
 __status__ = "Prototype"
 
-class PyiCloud:
-    def __init__ (self):
-        self.urlBase = "https://www.icloud.com"
-        self.urlAuth = "https://idmsa.apple.com"
-        self.urlSignIn = self.urlAuth + "/appleauth/auth/signin?widgetKey="
-        self.urlSetup = "https://setup.icloud.com/setup/ws/1"
-        self.urlKey = self.urlSetup + "/validate"
-        self.urlLogin = self.urlSetup + "/accountLogin"
-        self.mailBase = "@icloud.com"
+class Pointer:
+    def __init__ (self, value):
+        try:
+            self.value = value.value
+        except:
+            self.value = value
 
-        self.session = requests.Session()
-        self.response = None
+    def __call__ (self, value=None):
+        if value == None:
+            return self.value
+        try:
+            self.value = value.value
+        except:
+            self.value = value
+
+
+class HTTPService:
+    def __init__ (self, session, response=None, origin=None, referer=None):
         self.userAgent = "Python (X11; Linux x86_64)" 
-        self.accountName = None
-        self.appleWidgetKey = None
-        self.clientID = self.generateClientID()
+        try:
+            self.session = session.session
+            self.response = session.response
+            self.origin = session.origin 
+            self.referer = session.referer 
+        except:
+            session = session
+            self.response = response
+            self.origin = origin
+            self.referer = referer
+
+
+class IdmsaAppleService(HTTPService):
+    def __init__ (self, session):
+        super(IdmsaAppleService, self).__init__(session)
+        self.url = "https://idmsa.apple.com"
+        self.urlAuth = self.url + "/appleauth/auth/signin?widgetKey="
+
         self.appleSessionToken = None
 
-    def getAuthenticationRequestPayload (self, password):
-        return json({
-                "accountName": self.accountName,
-                "password": password,
-                "rememberMe": False
-                }) 
+    def requestAppleSessionToken (self, user, password, appleWidgetKey):
+        self.session.headers = self.getRequestHeader(appleWidgetKey)
+        self.response.value = self.session.post(self.urlAuth + appleWidgetKey,
+                self.getRequestPayload(user, password))
+        try:
+            self.appleSessionToken = self.response().headers["X-Apple-Session-Token"]
+        except Exception as e:
+            raise Exception("requestAppleSessionToken: Apple Session Token query failed", 
+                             self.urlAuth, repr(e))
+        return self.appleSessionToken
 
-    def getAuthenticationRequestHeader (self):
+    def getRequestHeader (self, appleWidgetKey):
+        if not appleWidgetKey:
+            raise NameError("getSetupQueryParameters: clientID not found")
         return {
                 "Accept": "application/json, text/javascript",
                 "Content-Type": "application/json",
                 "User-Agent": self.userAgent,
-                "X-Apple-Widget-Key": self.appleWidgetKey,
-                "X-Requested-With": "XMLHttpRequest"
+                "X-Apple-Widget-Key": appleWidgetKey,
+                "X-Requested-With": "XMLHttpRequest",
+                "Origin": self.origin,
+                "Referer": self.referer,
                 }
-    
-    def getSetupQueryParameters (self):
-        return {
-                "clientBuildNumber": "16CHotfix21",
-                "clientID": self.clientID,
-                "clientMasteringNumber": "16CHotfix21"
-               }
 
-    def getLoginRequestPayload (self):
-        if not self.appleSessionToken:
-            raise NameError("getLoginRequestPayload: X-Apple-ID-Session-Id unavailable")
+    def getRequestPayload (self, user, password):
+        if not user:
+            raise NameError("getAuthenticationRequestPayload: user not found")
+        if not password:
+            raise NameError("getAuthenticationRequestPayload: password not found")
         return json({
-                "dsWebAuthToken": self.appleSessionToken,
-                "extended_login": False
-                })
+            "accountName": user,
+            "password": password,
+            "rememberMe": False,
+            }) 
 
-    def getSetupRequestHeader (self):
+
+class SetupiCloudService(HTTPService):
+    def __init__ (self, session):
+        super(SetupiCloudService, self).__init__(session)
+        self.url = "https://setup.icloud.com/setup/ws/1"
+        self.urlKey = self.url + "/validate"
+        self.urlLogin = self.url + "/accountLogin"
+        
+        self.appleWidgetKey = None
+        self.cookies = None
+        self.dsid = None
+    
+    def requestAppleWidgetKey (self, clientID):
+        #self.urlBase + "/system/cloudos/16CHotfix21/en-us/javascript-packed.js"
+        self.session.headers = self.getRequestHeader()
+        self.response.value = self.session.get(self.urlKey, params=self.getSetupQueryParameters(clientID))
+        try:
+            self.appleWidgetKey = self.findQyery(self.response().text, "widgetKey=")
+        except Exception as e:
+            raise Exception("requestAppletWidgetKey: Apple Widget Key query failed", 
+                             self.urlKey, repr(e))
+        return self.appleWidgetKey
+     
+    def requestCookies (self, appleSessionToken, clientID):
+        self.session.headers = self.getRequestHeader()
+        self.response.value = self.session.post(self.urlLogin,
+                                          self.getLoginRequestPayload(appleSessionToken),
+                                          params=self.getSetupQueryParameters(clientID))
+        try:
+            self.cookies = self.response().headers["Set-Cookie"]
+        except Exception as e:
+            raise Exception("requestCookies: Cookies query failed", 
+                             self.urlLogin, repr(e))
+        try:
+            self.dsid = self.response().json()["dsInfo"]["dsid"]
+        except Exception as e:
+            raise Exception("requestCookies: dsid query failed", 
+                             self.urlLogin, repr(e))
+        return self.cookies, self.dsid
+
+    def findQyery (self, data, query):
+        response = ''
+        foundAt = data.find(query)
+        if foundAt == -1:
+            raise Exception("findQyery: " + query + " could not be found in data")
+        foundAt += len(query)
+        char = data[foundAt]
+        while char.isalnum():
+            response += char
+            foundAt += 1
+            char = data[foundAt]
+        return response 
+
+
+    def getRequestHeader (self):
         return {
                 "Accept": "*/*",
                 "Connection": "keep-alive",
                 "Content-Type": "text/plain",
-                "Origin": self.urlBase,
+                "User-Agent": self.userAgent,
+                "Origin": self.origin,
+                "Referer": self.referer,
+               }
+
+    def getSetupQueryParameters (self, clientID):
+        if not clientID:
+            raise NameError("getSetupQueryParameters: clientID not found")
+        return {
+                "clientBuildNumber": "16CHotfix21",
+                "clientID": clientID,
+                "clientMasteringNumber": "16CHotfix21",
+               }
+
+    def getLoginRequestPayload (self, appleSessionToken):
+        if not appleSessionToken:
+            raise NameError("getLoginRequestPayload: X-Apple-ID-Session-Id not found")
+        return json({
+                "dsWebAuthToken": appleSessionToken,
+                "extended_login": False,
+                })
+
+    
+class ICloudService(HTTPService):
+    def __init__ (self):
+        super(ICloudService, self).__init__(session)
+        self.url = "https://www.icloud.com"
+        self.urlApp = self.url + "/applications"
+
+class ReminderWidget (HTTPService):
+    def __init__ (self, session):
+        super(ReminderWidget, self).__init__()
+        self.session = session
+        self.urlReminder = self.urlApp + "/reminders/current/en-gb/index.html"
+
+    def getRequestHeader (self, cookie):
+        return {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Connection": "keep-alive",
+                "Cookie": cookie,
+                "Host": self.urlBase,
                 "Referer": self.urlBase,
+                "Upgrade-Insecure-Requests":"1",
                 "User-Agent": self.userAgent,
                }
+
+    def getReminderList (self, cookie):
+        self.session.headers.update(self.getRequestHeader(cookie))
+        response = self.session.get(self.urlReminder)
+        response.raise_for_status()
+        print(response.text)
+
+
+class PyiCloudService (HTTPService):
+    def __init__ (self):
+        self.session = requests.Session()
+        self.session.verify = True
+        self.response = Pointer(None)
+        self.origin = "https://www.icloud.com"
+        self.referer = "https://www.icloud.com"
+        super(PyiCloudService, self).__init__(self)
+
+        self._ESCAPE_CHAR = ",;&%!?|(){}[]~>*\'\"\\"
+
+        self.clientID = self.generateClientID()
+
+        self.idmsaApple = IdmsaAppleService(self)
+        self.setupiCloud = SetupiCloudService(self)
+
+    def login (self):
+        user = self.parseAccountName(input("User: "))
+        password = getpass.getpass()
+        self.initSession(user, password)
+
+    def initSession (self,user, password):
+        clientID = self.clientID = self.generateClientID()
+        print(clientID)
+        widgetKey = self.setupiCloud.requestAppleWidgetKey(clientID)
+        print(widgetKey)
+        sessionToken = self.idmsaApple.requestAppleSessionToken(user, password, widgetKey)
+        print(sessionToken)
+        cookies, dsid = self.setupiCloud.requestCookies(sessionToken, clientID)
+        return (cookies, dsid)
 
     def generateClientID (self):
         return str(generateClientID()).upper()
 
-
-class iCloudWidgets:
-    mail = "mail"
-    contacts = "contacts"
-    calendar = "calendar"
-    photos = "photos"
-    iCloudDrive = "iclouddrive"
-    notes = "notes"
-    reminders = "reminders"
-    pages = "pages"
-    numbers = "numbers"
-    keynote = "keynote"
-    findFriends = "findFriends"
-    findiPhone = "findiphone"
-    settings = "settings"
-
-
-class PyiCloudService (PyiCloud):
-    def __init__ (self):
-        super(PyiCloudService, self).__init__()
-        self._ESCAPE_CHAR = ",;&%!?|(){}[]~>*\'\"\\"
-
-    def requestAppleWidgetKey (self):
-        #self.urlBase + "/system/cloudos/16CHotfix21/en-us/javascript-packed.js"
-        self.session.headers.update(self.getSetupRequestHeader())
-        self.response = self.session.get(self.urlKey, params=self.getSetupQueryParameters())
-        print(self.response.status_code)
-        try:
-            self.appleWidgetKey = self.getAppleWidgetKey(self.response.text)
-        except Exception as e:
-            raise Exception("requestAppletWidgetKey: Apple Widget Key query failed", 
-                             self.urlKey,
-                             repr(e))
-        print(self.appleWidgetKey)
-        
-    def login (self):
-        self.accountName = self.parseAccountName(input("User: "))
-        password = getpass.getpass()
-        self.session.verify = True
-        self.session.headers.update(self.getAuthenticationRequestHeader())
-
-        self.response = self.session.post(self.urlSignIn + self.appleWidgetKey,
-                                          self.getAuthenticationRequestPayload(password))
-        self.response.raise_for_status()
-        print(self.response.headers)
-        self.appleSessionToken = self.response.headers["X-Apple-Session-Token"]
-
-        print(self.getLoginRequestPayload())
-        self.session.headers.update(self.getSetupRequestHeader())
-        self.response = self.session.post(self.urlLogin ,#+ self.getQueryString(self.getSetupQueryParameters()),
-                                          self.getLoginRequestPayload(),
-                                          params=self.getSetupQueryParameters())
-        print("\n\nHEADERS")
-        print(self.response.headers)
-        print("\n\nTEXT")
-        print(self.response.text)
-        print(self.response.status_code)
-        self.response.raise_for_status()
-        
     def parseAccountName (self, accountName):
         cleanAccountName = self.stripSpaces(self.cleanSpecialChar(accountName))
         if '@' not in cleanAccountName:
-            cleanAccountName += self.mailBase
+            cleanAccountName += "@icloud.com"
         return cleanAccountName
 
-    def getAppleWidgetKey (self, data):
-        widgetKey = ''
-        query = "widgetKey="
-        foundAt = data.find(query)
-        if foundAt == -1:
-            raise Exception("getAppleWidgetKey: Apple Widget Key could not be found")
-        foundAt += len(query)
-        char = data[foundAt]
-        while char.isalnum():
-            widgetKey += char
-            foundAt += 1
-            char = data[foundAt]
-        return widgetKey 
 
     def cleanSpecialChar (self, text):
         cleanText = text
